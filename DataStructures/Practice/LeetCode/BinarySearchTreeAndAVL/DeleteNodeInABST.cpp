b10ea84cc44b2b5d52f7c5636076d503a2f8c5f7
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2022-09-30 02:23:17 +0600

    Update DeleteNodeInABST.cpp

--- DataStructures/Practice/LeetCode/BinarySearchTreeAndAVL/DeleteNodeInABST.cpp
+++ DataStructures/Practice/LeetCode/BinarySearchTreeAndAVL/DeleteNodeInABST.cpp
@@ -12,34 +12,31 @@
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int val) {
-    if (!root) return root;
+    if (root==nullptr) return root;
     if (val < root->val) {
         root->left = deleteNode(root->left, val);
     } else if (val > root->val) {
         root->right = deleteNode(root->right, val);
     } else {
         /* Leaf node case */
-        if (!root->left && !root->right) {
-            delete(root);
+        if (root->left==nullptr && root->right==nullptr) {
+            delete root;
             return nullptr;
         }
         /* 1 child case */
-        if (!root->left ) {
+        if (root->left==nullptr ) {
             TreeNode *ret = root->right; 
-            delete(root);
+            delete root;
             return ret;
         }
-         else if (!root->right ) {
+         else if (root->right==nullptr ) {
             TreeNode *ret = root->left; 
-            delete(root);
+            delete root;
             return ret;
         }
         /* 2 child case */
-        if (root->left && root->right) {
-            TreeNode *tmp = root->right;
-            while (tmp->left) {
-                tmp = tmp->left;
-            }
+        if (root->left!=nullptr && root->right!=nullptr) {
+            TreeNode *tmp = InorderSuccessor(root->right);
             root->val = tmp->val;
             root->right = deleteNode(root->right, root->val);
         }
